baceb904a90b2c2b55c571194bdaab74b54d9d87
max-lt <maxime.tissier@gmail.com>
2026-02-11 12:54:37 +0100

    Update svelte adapter

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,12 +1,12 @@
 import adapter from '@openworkers/adapter-sveltekit';
+import type { Config } from '@sveltejs/kit';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({ functions: true, out: 'build' })
+    adapter: adapter({ functions: true, debug: { prettier: true } })
   }
 };
 
